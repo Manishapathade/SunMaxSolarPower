cd5cbef6dde804f3b905ceecfa4cfe57b86153c9
manisha pathade <manupatil291299@gmail.com>
2025-09-25 22:46:53 +0530

    sunmax project

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+
+const humburger = document.querySelector('.humburger');
+const mobileMenu = document.querySelector('.mobile-menu');
+const bars = document.querySelector('.fa-bars');
+
+
+humburger.addEventListener('click', ()=>mobileMenu.classList.toggle('mobile-menu-active'))
+    humburger.addEventListener('click', ()=>bars.classList.toggle('fa-xmark'))
+
+
+    
